23f3d8f195acf62009a239b5ff7cfec14ae5438d
Shorouk Elkhateeb <shrouca@gmail.com>
2022-01-09 10:48:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tic Tac Toe
+A 2-player Tic Tac Toe game built using JavaScript, CSS, and HTML
+
+## Source
+[Build Tic Tac Toe With JavaScript - Tutorial](https://www.youtube.com/watch?v=Y-GkMjUZsmM)
+
+## Author
+Shorouk Elkhateeb
---
+++ script.js
@@ -0,0 +1,96 @@
+const cellElements = document.querySelectorAll('[data-cell]');
+let circleTurn;
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+const board = document.getElementById('board');
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+
+restartButton.addEventListener('click', startGame);
+
+startGame();
+
+function startGame(){
+    circleTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove('x');
+        cell.classList.remove('circle');
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, {once: true})
+    });
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');   
+}
+
+function handleClick(e){
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS: X_CLASS;
+    //place mark
+    placeMark(cell, currentClass);
+
+    //check for win
+    if(checkWin(currentClass)){
+        endGame(false);
+    } //check for draw
+    else if(isDraw()){
+        endGame(true);
+    } else{
+        //swich turns
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass);
+}
+
+function swapTurns(){
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if(circleTurn){
+        board.classList.add(CIRCLE_CLASS);
+    }
+    else{
+        board.classList.add(X_CLASS);
+    }
+}
+
+function isDraw(){
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) ||
+        cell.classList.contains(CIRCLE_CLASS);
+    })
+}
+
+function checkWin(currentClass){
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        })
+    });
+}
+
+function endGame(draw){
+    if(draw){
+        winningMessageTextElement.innerText = 'Draw!';
+    } else{
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+    }
+    winningMessageElement.classList.add('show');
+}
